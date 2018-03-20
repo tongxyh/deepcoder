6e0177e8cad84453fb9b52f15977febff6a2a5de
Tong Chen <tongxyh@163.com>
2018-03-20 21:46:46 +0800

    add TRAIN_FLAG

--- model.py
+++ model.py
@@ -247,165 +247,165 @@ def deepcoder_rd_v1_10(in_gt,IMG_W,IMG_H,IMG_C):
     return q_x_norm,q_x,output
 
 #v31
-def deepcoder_bn(in_gt,BN_FLAG=True):
+def deepcoder_bn(in_gt,TRAIN_FLAG=True):
     conv_0_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_0_0")(in_gt)
-    bn_0_0 = tf.layers.batch_normalization(conv_0_0,training=BN_FLAG)
+    bn_0_0 = tf.layers.batch_normalization(conv_0_0,training=TRAIN_FLAG)
 
     conv_0_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_0_1")(bn_0_0)
-    bn_0_1 = tf.layers.batch_normalization(conv_0_1,training=BN_FLAG)
+    bn_0_1 = tf.layers.batch_normalization(conv_0_1,training=TRAIN_FLAG)
 
     pool_0 = tf.keras.layers.AveragePooling2D((2,2))(bn_0_1)
 
     conv_1_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_1_0")(pool_0)
-    bn_1_0 = tf.layers.batch_normalization(conv_1_0,training=BN_FLAG)
+    bn_1_0 = tf.layers.batch_normalization(conv_1_0,training=TRAIN_FLAG)
 
     conv_1_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_1_1")(bn_1_0)
-    bn_1_1 = tf.layers.batch_normalization(conv_1_1,training=BN_FLAG)
+    bn_1_1 = tf.layers.batch_normalization(conv_1_1,training=TRAIN_FLAG)
 
     pool_1 = tf.keras.layers.AveragePooling2D((2,2))(bn_1_1)
 
     conv_2_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_2_0")(pool_1)
-    bn_2_0 = tf.layers.batch_normalization(conv_2_0,training=BN_FLAG)
+    bn_2_0 = tf.layers.batch_normalization(conv_2_0,training=TRAIN_FLAG)
 
     conv_2_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_2_1")(bn_2_0)
-    bn_2_1 = tf.layers.batch_normalization(conv_2_1,training=BN_FLAG)
+    bn_2_1 = tf.layers.batch_normalization(conv_2_1,training=TRAIN_FLAG)
 
     pool_2 = tf.keras.layers.AveragePooling2D((2,2))(bn_2_1)
 
     conv_3_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_3_0")(pool_2)
-    bn_3_0 = tf.layers.batch_normalization(conv_3_0,training=BN_FLAG)
+    bn_3_0 = tf.layers.batch_normalization(conv_3_0,training=TRAIN_FLAG)
 
     conv_3_1 = tf.keras.layers.Conv2D(4, (3, 3), padding='same',activation='sigmoid',name = "Conv2D_3_1")(bn_3_0)
 
     q_x_norm , q_x = quantizer_norm(conv_3_1)
 
     dconv_0_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_0_0")(q_x)
-    dbn_0_0 = tf.layers.batch_normalization(dconv_0_0,training=BN_FLAG)
+    dbn_0_0 = tf.layers.batch_normalization(dconv_0_0,training=TRAIN_FLAG)
 
     dconv_0_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_0_1")(dbn_0_0)
-    dbn_0_1 = tf.layers.batch_normalization(dconv_0_1,training=BN_FLAG)
+    dbn_0_1 = tf.layers.batch_normalization(dconv_0_1,training=TRAIN_FLAG)
 
     up_0 = tf.keras.layers.UpSampling2D((2,2))(dbn_0_1)
 
     dconv_1_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_1_0")(up_0)
-    dbn_1_0 = tf.layers.batch_normalization(dconv_1_0,training=BN_FLAG)
+    dbn_1_0 = tf.layers.batch_normalization(dconv_1_0,training=TRAIN_FLAG)
 
     dconv_1_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_1_1")(dbn_1_0)
-    dbn_1_1 = tf.layers.batch_normalization(dconv_1_1,training=BN_FLAG)
+    dbn_1_1 = tf.layers.batch_normalization(dconv_1_1,training=TRAIN_FLAG)
 
     up_1 = tf.keras.layers.UpSampling2D((2,2))(dbn_1_1)
 
     dconv_2_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_2_0")(up_1)
-    dbn_2_0 = tf.layers.batch_normalization(dconv_2_0,training=BN_FLAG)
+    dbn_2_0 = tf.layers.batch_normalization(dconv_2_0,training=TRAIN_FLAG)
 
     dconv_2_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_2_1")(dbn_2_0)
-    dbn_2_1 = tf.layers.batch_normalization(dconv_2_1,training=BN_FLAG)
+    dbn_2_1 = tf.layers.batch_normalization(dconv_2_1,training=TRAIN_FLAG)
 
     up_2 = tf.keras.layers.UpSampling2D((2,2))(dbn_2_1)
     dconv_3_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_3_0")(up_2)
-    dbn_3_0 = tf.layers.batch_normalization(dconv_3_0,training=BN_FLAG)
+    dbn_3_0 = tf.layers.batch_normalization(dconv_3_0,training=TRAIN_FLAG)
 
     dconv_3_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_3_1")(dbn_3_0)
-    dbn_3_1 = tf.layers.batch_normalization(dconv_3_1,training=BN_FLAG)
+    dbn_3_1 = tf.layers.batch_normalization(dconv_3_1,training=TRAIN_FLAG)
 
     output = tf.keras.layers.Conv2D(3, (3, 3), padding='same',name = "dConv2D_3_2")(dbn_3_1)
     return q_x_norm,q_x,output
 
-def deepcoder_bn_encoder(in_gt,BN_FLAG=True):
+def deepcoder_bn_encoder(in_gt,TRAIN_FLAG=True):
     conv_0_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_0_0")(in_gt)
-    bn_0_0 = tf.layers.batch_normalization(conv_0_0,training=BN_FLAG)
+    bn_0_0 = tf.layers.batch_normalization(conv_0_0,training=TRAIN_FLAG)
 
     conv_0_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_0_1")(bn_0_0)
-    bn_0_1 = tf.layers.batch_normalization(conv_0_1,training=BN_FLAG)
+    bn_0_1 = tf.layers.batch_normalization(conv_0_1,training=TRAIN_FLAG)
 
     pool_0 = tf.keras.layers.AveragePooling2D((2,2))(bn_0_1)
 
     conv_1_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_1_0")(pool_0)
-    bn_1_0 = tf.layers.batch_normalization(conv_1_0,training=BN_FLAG)
+    bn_1_0 = tf.layers.batch_normalization(conv_1_0,training=TRAIN_FLAG)
 
     conv_1_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_1_1")(bn_1_0)
-    bn_1_1 = tf.layers.batch_normalization(conv_1_1,training=BN_FLAG)
+    bn_1_1 = tf.layers.batch_normalization(conv_1_1,training=TRAIN_FLAG)
 
     pool_1 = tf.keras.layers.AveragePooling2D((2,2))(bn_1_1)
 
     conv_2_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_2_0")(pool_1)
-    bn_2_0 = tf.layers.batch_normalization(conv_2_0,training=BN_FLAG)
+    bn_2_0 = tf.layers.batch_normalization(conv_2_0,training=TRAIN_FLAG)
 
     conv_2_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_2_1")(bn_2_0)
-    bn_2_1 = tf.layers.batch_normalization(conv_2_1,training=BN_FLAG)
+    bn_2_1 = tf.layers.batch_normalization(conv_2_1,training=TRAIN_FLAG)
 
     pool_2 = tf.keras.layers.AveragePooling2D((2,2))(bn_2_1)
 
     conv_3_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "Conv2D_3_0")(pool_2)
-    bn_3_0 = tf.layers.batch_normalization(conv_3_0,training=BN_FLAG)
+    bn_3_0 = tf.layers.batch_normalization(conv_3_0,training=TRAIN_FLAG)
 
     conv_3_1 = tf.keras.layers.Conv2D(4, (3, 3), padding='same',activation='sigmoid',name = "Conv2D_3_1")(bn_3_0)
 
     q_x_norm , q_x = quantizer_norm(conv_3_1)
     return q_x
 
-def deepcoder_bn_decoder_name(q_x,BN_FLAG=True):
+def deepcoder_bn_decoder_name(q_x,TRAIN_FLAG=True):
     dconv_0_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_0_0")(q_x)
-    dbn_0_0 = tf.layers.batch_normalization(dconv_0_0,training=BN_FLAG, name="dbn_0_0")
+    dbn_0_0 = tf.layers.batch_normalization(dconv_0_0,training=TRAIN_FLAG, name="dbn_0_0")
 
     dconv_0_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_0_1")(dbn_0_0)
-    dbn_0_1 = tf.layers.batch_normalization(dconv_0_1,training=BN_FLAG, name="dbn_0_1")
+    dbn_0_1 = tf.layers.batch_normalization(dconv_0_1,training=TRAIN_FLAG, name="dbn_0_1")
 
     up_0 = tf.keras.layers.UpSampling2D((2,2))(dbn_0_1)
 
     dconv_1_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_1_0")(up_0)
-    dbn_1_0 = tf.layers.batch_normalization(dconv_1_0,training=BN_FLAG, name="dbn_1_0")
+    dbn_1_0 = tf.layers.batch_normalization(dconv_1_0,training=TRAIN_FLAG, name="dbn_1_0")
 
     dconv_1_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_1_1")(dbn_1_0)
-    dbn_1_1 = tf.layers.batch_normalization(dconv_1_1,training=BN_FLAG, name="dbn_1_1")
+    dbn_1_1 = tf.layers.batch_normalization(dconv_1_1,training=TRAIN_FLAG, name="dbn_1_1")
 
     up_1 = tf.keras.layers.UpSampling2D((2,2))(dbn_1_1)
 
     dconv_2_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_2_0")(up_1)
-    dbn_2_0 = tf.layers.batch_normalization(dconv_2_0,training=BN_FLAG, name="dbn_2_0")
+    dbn_2_0 = tf.layers.batch_normalization(dconv_2_0,training=TRAIN_FLAG, name="dbn_2_0")
 
     dconv_2_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_2_1")(dbn_2_0)
-    dbn_2_1 = tf.layers.batch_normalization(dconv_2_1,training=BN_FLAG, name="dbn_2_1")
+    dbn_2_1 = tf.layers.batch_normalization(dconv_2_1,training=TRAIN_FLAG, name="dbn_2_1")
 
     up_2 = tf.keras.layers.UpSampling2D((2,2))(dbn_2_1)
     dconv_3_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_3_0")(up_2)
-    dbn_3_0 = tf.layers.batch_normalization(dconv_3_0,training=BN_FLAG, name="dbn_3_0")
+    dbn_3_0 = tf.layers.batch_normalization(dconv_3_0,training=TRAIN_FLAG, name="dbn_3_0")
 
     dconv_3_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_3_1")(dbn_3_0)
-    dbn_3_1 = tf.layers.batch_normalization(dconv_3_1,training=BN_FLAG, name="dbn_3_1")
+    dbn_3_1 = tf.layers.batch_normalization(dconv_3_1,training=TRAIN_FLAG, name="dbn_3_1")
 
     output = tf.keras.layers.Conv2D(3, (3, 3), padding='same',name = "dConv2D_3_2")(dbn_3_1)
     return output
 
-def deepcoder_bn_decoder(q_x,BN_FLAG=True):
+def deepcoder_bn_decoder(q_x,TRAIN_FLAG=True):
     dconv_0_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_0_0")(q_x)
-    dbn_0_0 = tf.layers.batch_normalization(dconv_0_0,training=BN_FLAG)
+    dbn_0_0 = tf.layers.batch_normalization(dconv_0_0,training=TRAIN_FLAG)
 
     dconv_0_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_0_1")(dbn_0_0)
-    dbn_0_1 = tf.layers.batch_normalization(dconv_0_1,training=BN_FLAG)
+    dbn_0_1 = tf.layers.batch_normalization(dconv_0_1,training=TRAIN_FLAG)
 
     up_0 = tf.keras.layers.UpSampling2D((2,2))(dbn_0_1)
 
     dconv_1_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_1_0")(up_0)
-    dbn_1_0 = tf.layers.batch_normalization(dconv_1_0,training=BN_FLAG)
+    dbn_1_0 = tf.layers.batch_normalization(dconv_1_0,training=TRAIN_FLAG)
 
     dconv_1_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_1_1")(dbn_1_0)
-    dbn_1_1 = tf.layers.batch_normalization(dconv_1_1,training=BN_FLAG)
+    dbn_1_1 = tf.layers.batch_normalization(dconv_1_1,training=TRAIN_FLAG)
 
     up_1 = tf.keras.layers.UpSampling2D((2,2))(dbn_1_1)
 
     dconv_2_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_2_0")(up_1)
-    dbn_2_0 = tf.layers.batch_normalization(dconv_2_0,training=BN_FLAG)
+    dbn_2_0 = tf.layers.batch_normalization(dconv_2_0,training=TRAIN_FLAG)
 
     dconv_2_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_2_1")(dbn_2_0)
-    dbn_2_1 = tf.layers.batch_normalization(dconv_2_1,training=BN_FLAG)
+    dbn_2_1 = tf.layers.batch_normalization(dconv_2_1,training=TRAIN_FLAG)
 
     up_2 = tf.keras.layers.UpSampling2D((2,2))(dbn_2_1)
     dconv_3_0 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name = "dConv2D_3_0")(up_2)
-    dbn_3_0 = tf.layers.batch_normalization(dconv_3_0,training=BN_FLAG)
+    dbn_3_0 = tf.layers.batch_normalization(dconv_3_0,training=TRAIN_FLAG)
 
     dconv_3_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same',activation='relu',name= "dConv2D_3_1")(dbn_3_0)
-    dbn_3_1 = tf.layers.batch_normalization(dconv_3_1,training=BN_FLAG)
+    dbn_3_1 = tf.layers.batch_normalization(dconv_3_1,training=TRAIN_FLAG)
 
     output = tf.keras.layers.Conv2D(3, (3, 3), padding='same',name = "dConv2D_3_2")(dbn_3_1)
     return output
